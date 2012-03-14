11b77fb2bbe99072a85eac7efa717960e865a0ad
Peter Hickey <peter.hickey@gmail.com>
2012-03-14 14:57:38 +1100

    Added documentation to BAM2MS.py on reads aligned to the reverse strand.

--- Python/BAM2MS.py
+++ Python/BAM2MS.py
@@ -10,6 +10,8 @@ import pysam
 ## Extract comethylation signal for reads overlapping multiple CpGs
 ## SAM input file is the output of Bismark (no pre-processing required)
 ## CpG overlap is as determined by Bismark output, i.e. only positions with Z or z in the methylation string are considered CpGs
+## This program is a re-write of SAM2MS_deprecated.py in order to utilise the pysam library. This version contains much additional functionality.
+
 
 ## This program is Copyright (C) 2012, Peter Hickey (hickey@wehi.edu.au)
 
@@ -28,24 +30,24 @@ import pysam
 
 
 # Order of fields in Bismark SAM file ###
- #####################################################################################
- ### (1) QNAME                                                                     ###
- ### (2) FLAG                                                                      ### 
- ### (3) RNAME                                                                     ###
- ### (4) POS                                                                       ###
- ### (5) MAPQ                                                                      ###
- ### (6) CIGAR                                                                     ###
- ### (7) RNEXT                                                                     ###
- ### (8) PNEXT                                                                     ###
- ### (9) TLEN                                                                      ###
- ### (10) SEQ                                                                      ###
- ### (11) QUAL                                                                     ###
- ### (12) NM-tag (edit distance to reference)                                      ###
- ### (13) XX-tag (base-by-base mismatches to the reference, not including indels)  ###
- ### (14) XM-tag (methylation call string)                                         ###
- ### (15) XR-tag (read conversion state for the alignment)                         ###
- ### (16) XG-tag (genome conversion state for the alignment)                       ###
-######################################################################################
+  #####################################################################################
+  ### (1) QNAME                                                                     ###
+  ### (2) FLAG                                                                      ### 
+  ### (3) RNAME                                                                     ###
+  ### (4) POS                                                                       ###
+  ### (5) MAPQ                                                                      ###
+  ### (6) CIGAR                                                                     ###
+  ### (7) RNEXT                                                                     ###
+  ### (8) PNEXT                                                                     ###
+  ### (9) TLEN                                                                      ###
+  ### (10) SEQ                                                                      ###
+  ### (11) QUAL                                                                     ###
+  ### (12) NM-tag (edit distance to reference)                                      ###
+  ### (13) XX-tag (base-by-base mismatches to the reference, not including indels)  ###
+  ### (14) XM-tag (methylation call string)                                         ###
+  ### (15) XR-tag (read conversion state for the alignment)                         ###
+  ### (16) XG-tag (genome conversion state for the alignment)                       ###
+  #####################################################################################
 
 # Explanation of methylation string (XM) 
   #################################################################
@@ -58,17 +60,18 @@ import pysam
   ### z for not methylated C in CpG context (was converted)     ###
   #################################################################
 
-# This program is a re-write of SAM2MS.py in order to utilise the pysam library.
+### Stranded-ness in Bismark SAM files
+# A read that aligns to the reverse strand has its read sequence reverse-complemented prior to being stored in the SEQ field of the SAM file (consistent with Bowtie)
+# This means that the 5' end of the original read (aka the 'good quality end') is at the leftmost position in the SEQ field regardless of which strand the read aligned against.
 
-## TODO: Pretty up the file and add error checks & warnings
-## TODO: Discuss "random choice of CpGs in a read" with Terry - naive approach results in pairs with intra-pair distance > 40 (< 30%). Prove this analytically. Might choose outermost pair.
-## TODO: Make shorter version of variable names, e.g. -5/--ignore5
-## TODO: Add counter for "ignored" CpGs, i.e. those skipped because of -3 or -5 arguments
-## TODO: Add check to make sure "ignore" values make sense, e.g. so you can't ignore "-3" bases or "50 bases of a 35bp read" etc.
-## TODO: Add option to ignore marked duplicates
-## TODO: Add SAM file support
-## TODO: Compute maxReadLength from data
-## TODO: Write Version 2 that looks at read content, rather than XM tag, to determine methylation status. NB: Will need to be very careful with reads aligning to Crick-strand (NB: unmethylated reverse strand reads are A at the G in the CpG and methylated reads are G at the G in the CpG.)
+### A note on duplicates
+# True PCR duplicates may be missed by MarkDuplicates if the reads have undergone different amounts of trimming since the start and end co-ordinates of the duplicate reads no longer need coincide even if both reads have the same POS field in the SAM file.
+
+## TODO: Discuss "random choice of CpGs in a read" with Terry - naive approach results in pairs with intra-pair distance > 40 (< 30%). Prove this analytically. Might choose outermost pair instead.
+## TODO: Add SAM file support (should be possible with upgrade of pysam from version 0.4.1 to 0.6)
+## TODO: Paired-end version. Be careful of orientation of read pairs and stranded-ness for ignore5, ignore3 options and parsing of XM tag
+
+## TODO: (MAJOR UPGRADE) Write Version 2 that looks at read content, rather than XM tag, to determine methylation status. NB: Will need to be very careful with reads aligning to Crick-strand (NB: unmethylated reverse strand reads are A at the G in the CpG and methylated reads are G at the G in the CpG.)
 
 # Command line passer
 parser = argparse.ArgumentParser(description='Extract the methylation calls for a CpG pair from reads that overlap multiple CpGs from a Bismark BAM file. The output file contains the positions of the CpG pair using 1-based co-ordinates of the cytosine in each CpG. If a read overlaps more than two CpGs there are several ways to construct the pairs (see the --pairChoice argument). The output of this file can be used for analysing comethylation along a read.')
@@ -76,19 +79,19 @@ parser.add_argument('BAM', metavar = 'BAM',
                   help='The path to the BAM file')
 parser.add_argument('output', type=argparse.FileType('w'), nargs=1,
                    help='The output filename')
-parser.add_argument('--ignore5', metavar = '<int>', type = int,
+parser.add_argument('-5', '--ignore5', metavar = '<int>', type = int,
                   default=0,
-                  help='Ignore <int> bases from 5\' (left) end of reads (default: 0)')
-parser.add_argument('--ignore3', metavar = '<int>', type = int,
+                  help='Ignore <int> bases from 5\' (left) end of reads (default: 0). WARNING: Parameter value not sanity checked by program.')
+parser.add_argument('-3', '--ignore3', metavar = '<int>', type = int,
                   default=0,
-                  help='Ignore <int> bases from 3\' (right) end of reads (default: 0)')
+                  help='Ignore <int> bases from 3\' (right) end of reads (default: 0). WARNING: Parameter value not sanity checked by program.')
 parser.add_argument('--maxReadLength', metavar = '<int>', type = int,
                   default=150,
                   help='Maximum read length in bp (default: 150)')
 parser.add_argument('--pairChoice', metavar = '<string>',
                   default="random",
                   help='Method for constructing CpG pairs in reads that contain more than two CpGs: random, leftmost or outermost (default: random)')
-parser.add_argument('--dummy', action='store_true',help='An example dummy TRUE/FALSE variable (default: FALSE)')
+parser.add_argument('--ignoreDuplicates', action='store_true',help='Ignore reads that have been flagged as PCR duplicates by Picard\'s MarkDuplicates function')
 
 args = parser.parse_args()
 
@@ -103,7 +106,8 @@ ignore3 = args.ignore3
 CpG_pattern = re.compile(r"[Zz]")
 methylated_CpG_pattern = re.compile(r"[Z]")
 unmethylated_CpG_pattern = re.compile(r"[z]")
-read_counter = 0 # The number of reads processed by SAM2MS.py
+read_counter = 0 # The number of reads processed by BAM2MS.py
+duplicate_read_counter = 0 # The number of duplicate reads ignore by BAM2MS.py
 CpGs_per_read = [0] * (maxReadLength/2 + 2) # The number of reads containing x CpGs (x = 0, 1, ...; the minimum number of CpGs is 0 and the maximum number of CpGs is half the read length + 1)
 CpG_positions = [0] * maxReadLength # Count how often each position on the read was a CpG
 methylated_CpG_positions = [0] * maxReadLength # Count how many times each position on the read was a methylated CpG
@@ -113,57 +117,66 @@ unmethylated_CpG_positions = [0] * maxReadLength # Count how many times each pos
 tabWriter = csv.writer(OUT, delimiter='\t', quotechar=' ', quoting=csv.QUOTE_MINIMAL)
 
 # Opens BAM file - does not currently work for SAM files as the autodetect SAM/BAM option isn't working for pysam.Samfile()
-f = pysam.Samfile(args.BAM, 'rb') 
+bam = pysam.Samfile(args.BAM, 'rb') 
 
 # Process each line of SAM file
-for read in f:
-    read_counter += 1 # Increment the counter
-    chrom = f.getrname(read.tid) # 
-    start = read.pos + 1 # read.pos is 0-based in accordance with BAM file specificiations and Python standards. I convert this to 1-based positions.
-    XM = [tag[1] for tag in read.tags if tag[0]=='XM'][0] # Tags are stored as a Python list of 2-tuples [("TAG_NAME", "TAG_VALUE"), ...]. This assumes there is one, and one only, XM tag.
-    CpG_index = [m.start() for m in re.finditer(CpG_pattern, XM)] # Stores index of CpGs in read (C for Watson strand, G for Crick strand)
-    methylated_CpG_index = [m.start() for m in re.finditer(methylated_CpG_pattern, XM)] # Stores index of methylated CpGs in read
-    unmethylated_CpG_index = [m.start() for m in re.finditer(unmethylated_CpG_pattern, XM)] # Stores index of unmethylated CpGs in read
-    n_CpGs = len(CpG_index) # The number of CpGs the read overlaps
-    CpGs_per_read[n_CpGs] += 1 # Record how many CpGs were covered by the read
-    # Increment the counters
-    for i in CpG_index:
-        CpG_positions[i] += 1
-    for i in methylated_CpG_index:
-        methylated_CpG_positions[i] += 1
-    for i in unmethylated_CpG_index:
-        unmethylated_CpG_positions[i] += 1
-    # Ignore --ignore5 and --ignore3 bases from read
-    CpG_index = [pos for pos in CpG_index if pos > ignore5 and pos <= (len(XM) - ignore3)]
-    n_CpGs = len(CpG_index) # Update the number of CpGs the read overlaps post-"ignore"
-    # If there is more than one CpG in the read then we want to process that read
-    if n_CpGs > 1:
-        # Create a CpG pair - the choice is random, leftmost or outermost.
-        # The outermost pair ensures the greatest intra-pair distance.
-        if pairChoice=="random":
-            CpG_pair = random.sample(CpG_index, 2)
-            CpG_pair.sort() # Important to sort the result to ensure CpG_1 < CpG_2 by position
-        elif pairChoice=="outermost":
-            CpG_pair = [CpG_index[0], CpG_index[-1]]
-        elif pairChoice=="leftmost":
-            CpG_pair = [CpG_index[0], CpG_index[1]]
-        else:
-            sys.exit("Error: pairChoice must be one of random, leftmost or outermost. Please retry.")
-        index = array(CpG_pair)
-        # If a read maps to the reverse strand the Z/z characters in the XM string point to the G in the CpG - I want to point to the C in the CpG so I move the position coordinates 1bp to the left
-        if(read.is_reverse):
-            positions = start + index - 1 # positions are 1-based positions of the cytosines in each CpG 
-        else:
-            positions = start + index # positions are 1-based positions of the cytosines in each CpG 
-        output=[chrom, positions[0], positions[1], XM[index[0]], XM[index[1]]]
-        tabWriter.writerow(output)
+for read in bam:
+    # Skip duplicate reads if requested
+    if args.ignoreDuplicates and read.is_duplicate:
+        read_counter += 1
+        duplicate_read_counter += 1
+        continue
+    else:
+        read_counter += 1 # Increment the counter
+        chrom = bam.getrname(read.tid) # 
+        start = read.pos + 1 # read.pos is 0-based in accordance with BAM file specificiations and Python standards. I convert this to 1-based positions.
+        XM = [tag[1] for tag in read.tags if tag[0]=='XM'][0] # Tags are stored as a Python list of 2-tuples [("TAG_NAME", "TAG_VALUE"), ...]. This assumes there is one, and one only, XM tag.
+        CpG_index = [m.start() for m in re.finditer(CpG_pattern, XM)] # Stores index of CpGs in read (C for Watson strand, G for Crick strand)
+        methylated_CpG_index = [m.start() for m in re.finditer(methylated_CpG_pattern, XM)] # Stores index of methylated CpGs in read
+        unmethylated_CpG_index = [m.start() for m in re.finditer(unmethylated_CpG_pattern, XM)] # Stores index of unmethylated CpGs in read
+        n_CpGs = len(CpG_index) # The number of CpGs the read overlaps
+        CpGs_per_read[n_CpGs] += 1 # Record how many CpGs were covered by the read
+        # Increment the counters
+        for i in CpG_index:
+            CpG_positions[i] += 1
+        for i in methylated_CpG_index:
+            methylated_CpG_positions[i] += 1
+        for i in unmethylated_CpG_index:
+            unmethylated_CpG_positions[i] += 1
+        # Ignore --ignore5 and --ignore3 bases from read
+        CpG_index = [pos for pos in CpG_index if pos > ignore5 and pos <= (len(XM) - ignore3)]
+        n_CpGs = len(CpG_index) # Update the number of CpGs the read overlaps post-"ignore"
+        # If there is more than one CpG in the read then we want to process that read
+        if n_CpGs > 1:
+            # Create a CpG pair - the choice is random, leftmost or outermost.
+            # The outermost pair ensures the greatest intra-pair distance.
+            if pairChoice=="random":
+                CpG_pair = random.sample(CpG_index, 2)
+                CpG_pair.sort() # Important to sort the result to ensure CpG_1 < CpG_2 by position
+            elif pairChoice=="outermost":
+                CpG_pair = [CpG_index[0], CpG_index[-1]]
+            elif pairChoice=="leftmost":
+                CpG_pair = [CpG_index[0], CpG_index[1]]
+            else:
+                sys.exit("Error: pairChoice must be one of random, leftmost or outermost. Please retry.")
+            index = array(CpG_pair)
+            # If a read maps to the reverse strand the Z/z characters in the XM string point to the G in the CpG - I want to point to the C in the CpG so I move the position coordinates 1bp to the left
+            if(read.is_reverse):
+                positions = start + index - 1 # positions are 1-based positions of the cytosines in each CpG 
+            else:
+                positions = start + index # positions are 1-based positions of the cytosines in each CpG 
+            output=[chrom, positions[0], positions[1], XM[index[0]], XM[index[1]]]
+            tabWriter.writerow(output)
 
 # Close the file connections
-f.close()
+bam.close()
 OUT.close()
 
 # Print summary statistics to standard output
 print '%d reads processed' % read_counter
+if args.ignoreDuplicates:
+    print '%d duplicate reads ignored' % duplicate_read_counter
+print '\nHistogram of CpGs per read'
 sys.stdout.write('x')
 sys.stdout.write('\t')
 sys.stdout.write('Count\n')
