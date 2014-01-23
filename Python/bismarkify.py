ffe8eb02723befdd4192c19b22f7803b0aaa194f
Peter Hickey <peter.hickey@gmail.com>
2014-01-23 11:15:59 +1100

    Licensed Comethylation under the GPL-2 license. This closes #22.

--- Python/bismarkify.py
+++ Python/bismarkify.py
@@ -5,6 +5,23 @@ import csv
 import pysam
 import warnings
 
+#### LICENSE ####
+## Copyright (C) 2012 - 2014 Peter Hickey (peter.hickey@gmail.com)
+
+## This file is part of Comethylation.
+
+## Comethylation is free software: you can redistribute it and/or modify it
+## under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 2 of the License, or
+## (at your option) any later version.
+## Comethylation is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+
+## You should have received a copy of the GNU General Public License
+## along with Comethylation.  If not, see <http://www.gnu.org/licenses/>.
+
 ### Explanation of XM-tag (methylation string) ###
 ############################################################################################################################################################################################
 # . for bases not involving cytosines                       
--- Python/create_test_data.py
+++ Python/create_test_data.py
@@ -3,6 +3,23 @@
 # Peter Hickey
 # 15/01/2013
 
+#### LICENSE ####
+## Copyright (C) 2012 - 2014 Peter Hickey (peter.hickey@gmail.com)
+
+## This file is part of Comethylation.
+
+## Comethylation is free software: you can redistribute it and/or modify it
+## under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 2 of the License, or
+## (at your option) any later version.
+## Comethylation is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+
+## You should have received a copy of the GNU General Public License
+## along with Comethylation.  If not, see <http://www.gnu.org/licenses/>.
+
 #### TODO ###
 # TODO: Need to manually check each type of read listed on GitHub issues tracker. 
 
