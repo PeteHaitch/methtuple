d60624d38b2b9483a596981150098ae8efb0685c
Tobias Sargeant <sargeant@wehi.edu.au>
2014-02-05 15:48:20 +1100

    testing stub; shorten comethylation_funcs to funds; import everything into comethylation script

--- comethylation/__init__.py
+++ comethylation/__init__.py
@@ -1,7 +1,12 @@
 __version_info__ = ('0','99','2')
 __version__ = '.'.join(__version_info__)
 
+import funcs
+import mtuple
+
 __all__ = [
-  '__version_info__',
-  '__version__'
+    '__version_info__',
+    '__version__',
+    'funcs',
+    'mtuple'
 ]
