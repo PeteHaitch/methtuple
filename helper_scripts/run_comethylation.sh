9ad0289cbcbd6a1be27faea4f6f1c4298758c481
Peter Hickey <peter.hickey@gmail.com>
2014-02-13 09:24:10 +1100

    run_comethylation.sh helper script now assumes comethylation is in your $PATH. Made removal of chromosome-level files dependent upon whether they were successfully appended to the equivalent genome-level file.

--- helper_scripts/run_comethylation.sh
+++ helper_scripts/run_comethylation.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-#### TODOs ####
-# TODO: Concatenate chromosome-level output to a single file
-
 #### DESCRIPTION ####
 # Peter Hickey (peter.hickey@gmail.com)
 # 04/02/2014
-# A example bash script for processing a BAM file with comethylation.py in a pseudo-parallel chromosome-by-chromsome fashion.
+# A example bash script for processing a BAM file with comethylation in a pseudo-parallel chromosome-by-chromsome fashion.
 
 #### WARNINGS #####
-# WARNING: This is not a robust, general purpose script. Rather it works for my particular setup and suggests how comethylation.py might be sped up by processing chromosome-level (or some other sub-genome level) units
+# WARNING: This is not a robust, general purpose script. Rather it works for my particular setup and suggests how comethylation might be sped up by processing chromosome-level (or some other sub-genome level) units
 # WARNING: Script will produce output as subdirectories of OUTDIR
 # WARNING: This script runs 1 job per chromosome and each job can use 10-20Gb of memory. Roughly, the larger chromosomes require more memory and more computational time.
 # WARNING: The SAMPLE_NAME variable must not contain the "." (period) character
-# REQUIRES: samtools and Rscript must be in $PATH
+# REQUIRES: comethylation, samtools and Rscript must be in your $PATH
 # REQUIRES: tabulate_hist.r to be in the same folder as run_comethylation.sh
 
 #### ALGORITHM ####
@@ -22,23 +19,22 @@
 #		(a) if (Single-end data): Continue because there is no need to sort single-end data
 # 		(b) if (Paired-end data): Sort the chromosome-level BAMs by queryname order
 # (2) For each m-tuple value:
-#		(a) Process each (sorted) chromosome-level BAM with comethylation.py with that m-tuple value. Each chromosome is processed as a background job. 
+#		(a) Process each (sorted) chromosome-level BAM with comethylation with that m-tuple value. Each chromosome is processed as a background job. 
 #		(b) Do not move onto the next m-tuple value until all chromosome jobs finish.
 #		(c) Concetanate all chromosome-level m-tuples files
 
 #### Parameters ####
 BAM= # The path of the BAM file, e.g. "ex.bam". The BAM must be sorted in coordinate order and indexed. 
 OUTDIR= # The path to the directory used for output, e.g. "my_outdir". Multiple subdirectories and files will be created in this folder.
-COMETH= # The path to the script comethylation.py
 SAMPLE_NAME= # The sample name, which will be the prefix of all output files, e.g. "exciting_sample". WARNING: Must not contain the "." (period) character
 M= # A bash array of the "m" in m-tuples, e.g. (1 2 3).
 CHROMS= # A bash array of the chromosomes to be processed. It is recommended, but not required, that this array be in karyotypic order e.g. (chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chr10 chr11 chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chr20 chr21 chr22 chrX chrY chrM chrL).
-COMETH_PARAMS= # Additional parameters to be passed to comethylation.py, e.g. '--methylationType CG --ignoreDuplicates --minQual 0 --ignoreStart_r1 5 --ignoreStart_r2 $3 --ignoreEnd_r1 10 --ignoreEnd_r2 3 --methylationType CG --overlappingPairedEndFilter XM'.
+COMETH_PARAMS= # Additional parameters to be passed to comethylation e.g. '--methylationType CG --ignoreDuplicates --minQual 0 --ignoreStart_r1 5 --ignoreStart_r2 $3 --ignoreEnd_r1 10 --ignoreEnd_r2 3 --methylationType CG --overlappingPairedEndFilter XM'.
 SEQ_TYPE= # "SE" (single-end data) or "PE" (paired-end data).
 
 #### Step 0 ####
 # Check parameters
-if [[ -n "${BAM}" && -n "${OUTDIR}" && -n "${COMETH}" && -n "${SAMPLE_NAME}" && -n "${M}" && -n "${CHROMS}" && -n "${COMETH_PARAMS}" && -n "${SEQ_TYPE}" ]]
+if [[ -n "${BAM}" && -n "${OUTDIR}" && -n "${SAMPLE_NAME}" && -n "${M}" && -n "${CHROMS}" && -n "${COMETH_PARAMS}" && -n "${SEQ_TYPE}" ]]
 then
 	if [[ "$SAMPLE_NAME" != "${SAMPLE_NAME//./}" ]]
 	then
@@ -47,11 +43,10 @@ then
 	else
 		echo "BAM file = ${BAM}"
 		echo "Output directory = ${OUTDIR}"
-		echo "Path to comethylation.py = ${COMETH}"
 		echo "Sample name = ${SAMPLE_NAME}"
 		echo "'m' in m-tuples = ${M[@]}"
 		echo "Chromosomes = ${CHROMS[@]}"
-		echo "Additional comethylation.py parameters = ${COMETH_PARAMS}"
+		echo "Additional comethylation parameters = ${COMETH_PARAMS}"
 		echo "Sequencing type = ${SEQ_TYPE}"
 	fi
 
@@ -99,7 +94,7 @@ do
 	## Extract methylation loci m-tuples for each chromosome
 	for CHROM in ${CHROMS[@]}
 	do      
-		CMD="python ${COMETH} ${COMETH_PARAMS} --mTuple ${m} ${OUTDIR}/${SAMPLE_NAME}_${CHROM}.bam ${OUTDIR}/${m}_tuples/${SAMPLE_NAME}_${CHROM} &> ${OUTDIR}/${m}_tuples/log/${SAMPLE_NAME}_${CHROM}.${m}.log &"
+		CMD="comethylation ${COMETH_PARAMS} --mTuple ${m} ${OUTDIR}/${SAMPLE_NAME}_${CHROM}.bam ${OUTDIR}/${m}_tuples/${SAMPLE_NAME}_${CHROM} &> ${OUTDIR}/${m}_tuples/log/${SAMPLE_NAME}_${CHROM}.${m}.log &"
 		eval ${CMD}
 	done
 	# Wait until all chromosomes have finished before moving to next m-tuple
@@ -115,10 +110,10 @@ do
 	Rscript tabulate_hist.r ${OUTDIR}/${m}_tuples/hist
 	for CHROM in ${CHROMS[@]}
 	do      
-		cat ${OUTDIR}/${m}_tuples/${SAMPLE_NAME}_${CHROM}.${EXTENSION} | grep -v -P  "^chr\tpos1" >> ${OUTDIR}/${m}_tuples/${SAMPLE_NAME}.${EXTENSION} # Skip the header
-		rm ${OUTDIR}/${m}_tuples/${SAMPLE_NAME}_${CHROM}.${EXTENSION}
-		cat ${OUTDIR}/${m}_tuples/${SAMPLE_NAME}_${CHROM}.reads_that_failed_QC.txt >> ${OUTDIR}/${m}_tuples/${SAMPLE_NAME}.reads_that_failed_QC.txt
-		rm ${OUTDIR}/${m}_tuples/${SAMPLE_NAME}_${CHROM}.reads_that_failed_QC.txt
+		cat ${OUTDIR}/${m}_tuples/${SAMPLE_NAME}_${CHROM}.${EXTENSION} | grep -v -P  "^chr\tpos1" \ 
+		>> ${OUTDIR}/${m}_tuples/${SAMPLE_NAME}.${EXTENSION} && rm ${OUTDIR}/${m}_tuples/${SAMPLE_NAME}_${CHROM}.${EXTENSION}
+		cat ${OUTDIR}/${m}_tuples/${SAMPLE_NAME}_${CHROM}.reads_that_failed_QC.txt \ 
+		>> ${OUTDIR}/${m}_tuples/${SAMPLE_NAME}.reads_that_failed_QC.txt && rm ${OUTDIR}/${m}_tuples/${SAMPLE_NAME}_${CHROM}.reads_that_failed_QC.txt
 	done
 	# The final .tsv and "reads_that_failed_QC.txt" files are gzipped
 	gzip ${OUTDIR}/${m}_tuples/${SAMPLE_NAME}.${EXTENSION}
