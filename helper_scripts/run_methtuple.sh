51fd754891e5e476f22b16151bfada4fdb98bf27
Peter Hickey <peter.hickey@gmail.com>
2014-08-28 12:17:59 +1000

    Tidying of helper_scripts/run_methtuple.sh. Remove tmp files at end of script.

--- helper_scripts/run_methtuple.sh
+++ helper_scripts/run_methtuple.sh
@@ -3,7 +3,7 @@
 # DESCRIPTION
 #-----------------------------------------------------------------
 # Peter Hickey (peter.hickey@gmail.com)
-# 22/09/2014
+# 28/08/2014
 # A example bash script for processing a BAM file with methtuple
 # in parallelon a chromosome-by-chromsome basis.
 # This script makes extensive use of GNU parallel
@@ -50,7 +50,11 @@
 
 # NOTES
 #-------------------------------------------------------------------------------
-# NOTE: You may wish to increase the maximum memory per thread when running samtools sort via the -m flag. I have not explored the effects of increasing the number of threads available for sorting and compression (via the -@ flag); in particular, I do not know whether this plays nicely with GNU parallel.
+# NOTE: You may wish to increase the maximum memory per thread when running
+# samtools sort via the -m flag. I have not explored the effects of increasing
+# the number of threads available for sorting and compression (via the -@
+# flag); in particular, I do not know whether this plays nicely with GNU
+# parallel.
 
 # Required variables
 #-------------------------------------------------------------------------------
@@ -97,17 +101,18 @@ TABULATE_HIST=
 # Step 0
 #-------------------------------------------------------------------------------
 # Check variables
-if [[ -n "${BAM}" && -n "${OUTDIR}" && -n "${M}" && -n "${CHROMS}" && -n "${METHTUPLE_OPTIONS}" && -n "${SEQ_TYPE}" ]]
+if [[ -n "${BAM}" && -n "${OUTDIR}" && -n "${M}" && -n "${CHROMS}" \
+  && -n "${METHTUPLE_OPTIONS}" && -n "${SEQ_TYPE}" ]]
 then
-        echo "BAM file = ${BAM}"
-        echo "Output directory = ${OUTDIR}"
-        echo "'m' in m-tuples = ${M[@]}"
-        echo "Chromosomes = ${CHROMS[@]}"
-        echo "Additional methtuple options = ${METHTUPLE_OPTIONS}"
-        echo "Sequencing type = ${SEQ_TYPE}"
+  echo "BAM file = ${BAM}"
+  echo "Output directory = ${OUTDIR}"
+  echo "'m' in m-tuples = ${M[@]}"
+  echo "Chromosomes = ${CHROMS[@]}"
+  echo "Additional methtuple options = ${METHTUPLE_OPTIONS}"
+  echo "Sequencing type = ${SEQ_TYPE}"
 else
-        echo "Please check all parameters are valid"
-        exit
+  echo "Please check all parameters are valid"
+  exit
 fi
 SAMPLE_NAME=$(basename "$BAM")
 SAMPLE_NAME="${SAMPLE_NAME%.*}"
@@ -118,54 +123,89 @@ SAMPLE_NAME="${SAMPLE_NAME%.*}"
 # Create chromosome-level BAM files (queryname-sorted if paired-end sequencing)
 if [ ${SEQ_TYPE} == 'PE' ]
 then
-        echo "Paired-end sequencing"
-        echo "Creating chromosome-level BAMs and sorting by queryname"
-        parallel --joblog create_chromosome_level_BAMs.log -j ${NUM_CORES} "samtools view -u ${BAM} {1} | samtools sort -n - ${OUTDIR}/${SAMPLE_NAME}_{1}" ::: ${CHROMS[@]}
+  echo "Paired-end sequencing"
+  echo "Creating chromosome-level BAMs and sorting by queryname"
+  parallel --joblog create_chromosome_level_BAMs.log -j ${NUM_CORES} "
+  samtools view -u ${BAM} {1} | samtools sort -n - \
+  ${OUTDIR}/${SAMPLE_NAME}_{1}" ::: ${CHROMS[@]}
 elif [ ${SEQ_TYPE} == 'SE'  ]
 then
-        echo "Single-end sequencing"
-        echo "Creating chromosome-level BAMs"
-        parallel --joblog create_chromosome_level_BAMs.log -j ${NUM_CORES} "samtools view -u ${BAM} {1} > ${OUTDIR}/${SAMPLE_NAME}_{1}.bam" ::: ${CHROMS[@]}
+  echo "Single-end sequencing"
+  echo "Creating chromosome-level BAMs"
+        parallel --joblog create_chromosome_level_BAMs.log -j ${NUM_CORES} "
+        samtools view -u ${BAM} {1} > ${OUTDIR}/${SAMPLE_NAME}_{1}.bam" \
+        ::: ${CHROMS[@]}
 else
-        echo "The sequencing type (SEQ_TYPE parameter) must be one of 'SE' (for single-end sequencing) or 'PE' (paired-end for paired-end sequencing)"
-        exit
+  echo "The sequencing type (SEQ_TYPE parameter) must be one of 'SE' (for \
+  single-end sequencing) or 'PE' (paired-end for paired-end sequencing)"
+  exit
 fi
 #-------------------------------------------------------------------------------
 
 # Step 2
 #-------------------------------------------------------------------------------
 # mkdir for output
-parallel -j ${NUM_CORES} "mkdir -p ${OUTDIR}/{1}_tuples/log ${OUTDIR}/{1}_tuples/hist" ::: ${M[@]}
+parallel -j ${NUM_CORES} "mkdir -p ${OUTDIR}/{1}_tuples/log \
+${OUTDIR}/{1}_tuples/hist" ::: ${M[@]}
+
 # Extract methylation loci m-tuples for each chromosome
 echo "Extracting methylation loci m-tuples for each chromosome..."
-parallel --joblog methtuple.log -j ${NUM_CORES} "methtuple ${METHTUPLE_OPTIONS} -m {1} -o {1}_tuples/${SAMPLE_NAME}_{2} ${OUTDIR}/${SAMPLE_NAME}_{2}.bam &> ${OUTDIR}/{1}_tuples/log/${SAMPLE_NAME}_{1}.{2}.log" ::: ${M[@]} ::: ${CHROMS[@]}
-# Concatenate all chromosome-level data at the genome level. The only chromosome-level files that are retained are the .hist files.
+parallel --joblog methtuple.log -j ${NUM_CORES} "methtuple \
+${METHTUPLE_OPTIONS} -m {1} -o {1}_tuples/${SAMPLE_NAME}_{2} \
+${OUTDIR}/${SAMPLE_NAME}_{2}.bam &> \
+${OUTDIR}/{1}_tuples/log/${SAMPLE_NAME}_{1}.{2}.log" ::: ${M[@]} ::: \
+${CHROMS[@]}
+
+# The only chromosome-level files that are retained are the .hist files.
 echo "Concatenating chromosome-level files into genome-level files..."
-# Move all .hist files to a 'hist' directory and create the genome-wide .hist file.
-parallel -j ${NUM_CORES} "mv ${OUTDIR}/{1}_tuples/*.hist ${OUTDIR}/{1}_tuples/hist;
+# Move all .hist files to a 'hist' directory and create the genome-wide
+# .hist file.
+parallel -j ${NUM_CORES} "mv ${OUTDIR}/{1}_tuples/*.hist \
+${OUTDIR}/{1}_tuples/hist;
 Rscript ${TABULATE_HIST} ${SAMPLE_NAME} ${OUTDIR}/{1}_tuples/hist" ::: ${M[@]}
-# Create the header for the genome-wide .tsv file (for given m)
-parallel -j ${NUM_CORES} "EXTENSION=\$(basename ${OUTDIR}/{1}_tuples/${SAMPLE_NAME}_${CHROMS[0]}.*.tsv | rev | cut -d '.' -f -3 | rev);
-head -n 1 ${OUTDIR}/{1}_tuples/${SAMPLE_NAME}_${CHROMS[0]}.\${EXTENSION} > ${OUTDIR}/{1}_tuples/${SAMPLE_NAME}.\${EXTENSION}" ::: ${M[@]} # Note the need to escape the $ character for variables defined within the command to avoid the shell interpreting them (i.e. EXTENSION)
-# Concatenate the .tsv files and gzip (for given m)
-parallel --joblog cat_tsvs.log -j ${NUM_CORES} "EXTENSION=\$(basename ${OUTDIR}/{1}_tuples/${SAMPLE_NAME}_${CHROMS[0]}.*.tsv | rev | cut -d '.' -f -3 | rev);
-        for CHROM in ${CHROMS[@]}
-	do
-		cat ${OUTDIR}/{1}_tuples/${SAMPLE_NAME}_\${CHROM}.\${EXTENSION} | grep -v -P  '^chr\tstrand' >> ${OUTDIR}/{1}_tuples/${SAMPLE_NAME}.\${EXTENSION} && rm ${OUTDIR}/{1}_tuples/${SAMPLE_NAME}_\${CHROM}.\${EXTENSION}
-	done
-        gzip ${OUTDIR}/{1}_tuples/${SAMPLE_NAME}.\${EXTENSION}" ::: ${M[@]} # Note the need to escape the $ character for variables defined within the command executed to avoid the shell interpreting them (i.e. EXTENSION and CHROM)
-# Concatenate the reads_that_failed_QC files and gzip (for given m)
+
+# Create the header for the genome-wide .tsv file (for given m).
+# Note the need to escape the $ character for variables defined within the
+# command to avoid the shell interpreting them (i.e. EXTENSION)
+parallel -j ${NUM_CORES} "
+EXTENSION=\$(basename ${OUTDIR}/{1}_tuples/${SAMPLE_NAME}_${CHROMS[0]}.*.tsv \
+| rev | cut -d '.' -f -3 | rev);
+head -n 1 ${OUTDIR}/{1}_tuples/${SAMPLE_NAME}_${CHROMS[0]}.\${EXTENSION} > \
+${OUTDIR}/{1}_tuples/${SAMPLE_NAME}.\${EXTENSION}" ::: ${M[@]}
+
+# Concatenate the .tsv files and gzip (for given m).
+# Note the need to escape the $ character for variables defined within the
+# command executed to avoid the shell interpreting them (i.e. EXTENSION and
+# CHROM)
+parallel --joblog cat_tsvs.log -j ${NUM_CORES} "
+EXTENSION=\$(basename ${OUTDIR}/{1}_tuples/${SAMPLE_NAME}_${CHROMS[0]}.*.tsv \
+| rev | cut -d '.' -f -3 | rev);
+for CHROM in ${CHROMS[@]}
+  do
+		cat ${OUTDIR}/{1}_tuples/${SAMPLE_NAME}_\${CHROM}.\${EXTENSION}  \
+    | grep -v -P  '^chr\tstrand' >> \
+    ${OUTDIR}/{1}_tuples/${SAMPLE_NAME}.\${EXTENSION}
+  done
+gzip ${OUTDIR}/{1}_tuples/${SAMPLE_NAME}.\${EXTENSION}" ::: ${M[@]}
+
+# Concatenate the reads_that_failed_QC files and gzip (for given m).
+# Note the need to escape the $ character for variables defined within the
+# command executed to avoid the shell interpreting them (i.e. CHROM)
 parallel --joblog cat_reads_that_failed_QC_files.log -j ${NUM_CORES} "
-	for CHROM in ${CHROMS[@]}
-	do
-		cat ${OUTDIR}/{1}_tuples/${SAMPLE_NAME}_\${CHROM}.reads_that_failed_QC.txt >> ${OUTDIR}/{1}_tuples/${SAMPLE_NAME}.reads_that_failed_QC.txt && rm ${OUTDIR}/{1}_tuples/${SAMPLE_NAME}_\${CHROM}.reads_that_failed_QC.txt
+for CHROM in ${CHROMS[@]}
+  do
+		cat ${OUTDIR}/{1}_tuples/${SAMPLE_NAME}_\${CHROM}.reads_that_failed_QC.txt \
+    >> ${OUTDIR}/{1}_tuples/${SAMPLE_NAME}.reads_that_failed_QC.txt
 	done
-        gzip ${OUTDIR}/{1}_tuples/${SAMPLE_NAME}.reads_that_failed_QC.txt" ::: ${M[@]}
+gzip ${OUTDIR}/{1}_tuples/${SAMPLE_NAME}.reads_that_failed_QC.txt" ::: ${M[@]}
 #-------------------------------------------------------------------------------
 
 # Step 3
 #-------------------------------------------------------------------------------
 # Tidy up by removing chromosome-level BAM files
-echo "Removing chromosome-level BAMs..."
+echo "Removing chromosome-level files..."
 parallel -j ${NUM_CORES} "rm ${OUTDIR}/${SAMPLE_NAME}_{1}.bam" ::: ${CHROMS[@]}
+parallel -j ${NUM_CORES} "
+rm ${OUTDIR}/{1}_tuples/${SAMPLE_NAME}_*.tsv;
+rm ${OUTDIR}/{1}_tuples/${SAMPLE_NAME}_*.reads_that_failed_QC.txt" ::: ${M[@]}
 #-------------------------------------------------------------------------------
